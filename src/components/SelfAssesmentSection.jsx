d577bfaa65b184f4c578bf1694a9c4e5482d9adb
nachospreafico <nachospreafico06@gmail.com>
2023-05-04 15:03:34 +0200

    otros varios cambios

--- src/components/SelfAssesmentSection.jsx
+++ src/components/SelfAssesmentSection.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import { createWidget } from "@typeform/embed";
+
 import "./styles/SelfAssesmentSection.css";
 
 const SelfAssesmentSection = () => {
@@ -20,7 +20,6 @@ const SelfAssesmentSection = () => {
         Hace nuestra Autoevaluacion para que te recomendemos el programa que
         mejor se adapte a ti!
       </p>
-      <div className="typeform-form" ref={typeformContainer}></div>
     </div>
   );
 };
